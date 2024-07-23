538e4cac77e2ea2251789ff55c5823c956139f23
Bijan Patel <qascript20@gmail.com>
2024-07-23 11:59:20 +0100

    Add ChangeOrientation

--- src/test/java/android/ChangeOrientation.java
+++ src/test/java/android/ChangeOrientation.java
@@ -1,46 +1,45 @@
+package android;
+
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
 import org.openqa.selenium.ScreenOrientation;
+import org.openqa.selenium.print.PrintOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.ScreenshotException;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import java.io.File;
-import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Date;
 
 public class ChangeOrientation {
 
-    private AndroidDriver driver;
-    private static final String APPIUM_SERVER_URL = "http://127.0.0.1:4723";
+    public AndroidDriver driver;
+    ScreenOrientation currentOrientation;
 
     @BeforeTest
     public void setup() throws MalformedURLException {
+        String appiumServerUrl = "http://127.0.0.1:4723";
 
         DesiredCapabilities dc = new DesiredCapabilities();
         dc.setCapability("platformName","Android");
         dc.setCapability("appium:automationName","uiautomator2");
         dc.setCapability("appium:app",System.getProperty("user.dir")+ "/apps/ApiDemos.apk");
 
-        driver = new AndroidDriver(new URL(APPIUM_SERVER_URL),dc);
+        driver = new AndroidDriver(new URL(appiumServerUrl),dc);
     }
 
     @Test
-    public void test() throws IOException {
-        ScreenOrientation currentOrientation = driver.getOrientation();
+    public void test(){
+        currentOrientation = driver.getOrientation();
         System.out.println(currentOrientation);
-        if (currentOrientation != ScreenOrientation.LANDSCAPE) {
-            driver.rotate(ScreenOrientation.LANDSCAPE);
-        }
+        driver.rotate(ScreenOrientation.LANDSCAPE);
+        currentOrientation = driver.getOrientation();
+        System.out.println(currentOrientation);
     }
-    @AfterTest
-    public void close(){
-        driver.quit();
-    }
+
+//    @AfterTest
+//    public void close(){
+//        driver.quit();
+//    }
 }
