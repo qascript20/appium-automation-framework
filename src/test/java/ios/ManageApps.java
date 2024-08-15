bc6672e30160d69c172ad0ca032e3e0e574f58bd
Bijan Patel <qascript20@gmail.com>
2024-08-15 16:36:11 +0100

    Add ManageApps

--- src/test/java/ios/ManageApps.java
+++ src/test/java/ios/ManageApps.java
@@ -1,43 +1,50 @@
+package ios;
+
 import io.appium.java_client.ios.IOSDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class ManageApps {
 
-    public IOSDriver driver;
+    private IOSDriver driver;
+
+    private static final String APPIUM_SERVER_URL = "http://127.0.0.1:4723";
+
+    private static final String APP_PATH = System.getProperty("user.dir")+ "/apps/UIKitCatalog.app";
 
     @BeforeTest
     public void setup() throws MalformedURLException {
-        String appiumServerUrl = "http://127.0.0.1:4723";
 
         DesiredCapabilities dc = new DesiredCapabilities();
         dc.setCapability("platformName","IOS");
         dc.setCapability("appium:automationName","XCUITest");
-        dc.setCapability("appium:app",System.getProperty("user.dir")+ "/apps/UIKitCatalog.app");
         dc.setCapability("appium:deviceName","iPhone 15 Pro");
 
-        driver = new IOSDriver(new URL(appiumServerUrl),dc);
-
+        driver = new IOSDriver(new URL(APPIUM_SERVER_URL),dc);
     }
 
     @Test
-    public void test(){
-        //Is App Installed
-        boolean appInstalled = driver.isAppInstalled("com.example.apple-samplecode.UICatalog");
-        System.out.println(appInstalled);
+    public void isAppInstalled(){
 
-        //Remove the app
+        boolean installed = driver.isAppInstalled("com.example.apple-samplecode.UICatalog");
+        System.out.println(installed);
+    }
+
+    @Test
+    public void installApp(){
+
+        driver.installApp(APP_PATH);
+    }
+
+    @Test
+    public void removeApp(){
+
         driver.removeApp("com.example.apple-samplecode.UICatalog");
-
-        //Install the app
-        driver.installApp(System.getProperty("user.dir")+ "/apps/UIKitCatalog.app");
-
     }
 
     @AfterTest
